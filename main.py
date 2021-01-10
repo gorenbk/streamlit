a40641404fdfb1c95464bed5d753c8537ff8c79e
Goren Barak <gorenemail@gmail.com>
2021-01-10 11:57:15 -0500

    updated main.py and added main.json

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-import streamlit as st
-st.warning("Something is wrong.")
+try:
+    import streamlit as st
+except ModuleNotFoundError:
+    pass
+import json as jn
+def build_account_dict(**info):
+    return info
+jfile_read = open("main.json","r")
+jfile_write = open("main.json","w")
+age  = st.slider("What is your age?",1,120)
+name = st.text_input("What is your username?")
+account = build_account_dict(age=age,username=name)
+jn.dump(account,jfile_write)
+"""
+[View now on github](https://github.com/gorenbk/streamlit)
+"""
