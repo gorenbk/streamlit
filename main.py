226fc7a47332f9f5a9e79a5ebef92c78243efe3c
Goren Barak <gorenemail@gmail.com>
2021-01-10 12:28:17 -0500

    added some cool stuff

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-try:
-    import streamlit as st
-except ModuleNotFoundError:
-    pass
+import streamlit as st
 import json as js
 def account_dict(**info):
     return info
-json_read = open('main.json','r')
-json_write = open('main.json','w')
-age = st.slider("What is your age?",5,120)
-js.dump(age,json_write)
+with open('main.json','r') as json_read:
+    account = js.load(json_read)
+age = st.slider("What is your age?",5,120,account['age'])
+username = st.text_input("What is your username?",account['name'])
+with open('main.json','w') as json_write:
+    account = account_dict(age=age,
+    name=username)
+    js.dump(account,json_write)
+file_read = open('main.py','r')
